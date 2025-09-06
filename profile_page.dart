e6585951abc21c6c185a5c73e10e78d4a0754c36
TrangFluke <131250430+TrangFluke@users.noreply.github.com>
2025-09-07 00:09:15 +0700

    Create profile_page.dart

---
+++ profile_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('โปรไฟล์ติวเตอร์')),
+      body: Container(
+        color: const Color(0xFFFFE4E1), // สีไข่ชมพู
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const SizedBox(height: 60),
+            Center(
+              child: Image.asset(
+                'assets/logo.png',
+                width: 100,
+              ),
+            ),
+          ], // หน้าเปล่า
+        ),
+      ),
+    );
+  }
+}
